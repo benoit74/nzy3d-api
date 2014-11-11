75512616a7f6d6c3e6ef2110997ba26335e3831c
benoit74 <benoit.beraud@oviles.info>
2014-11-11 15:40:08 +0100

    Very simple WPF demo

---
+++ nzy3d-wpfDemo/MainWindow.xaml.cs
@@ -0,0 +1,103 @@
+﻿using nzy3D.Chart;
+using nzy3D.Colors;
+using nzy3D.Colors.ColorMaps;
+using nzy3D.Maths;
+using nzy3D.Plot3D.Builder;
+using nzy3D.Plot3D.Builder.Concrete;
+using nzy3D.Plot3D.Primitives;
+using nzy3D.Plot3D.Rendering.Canvas;
+using nzy3D.Plot3D.Rendering.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+
+namespace nzy3d_wpfDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        private nzy3D.Chart.Controllers.Thread.Camera.CameraThreadController t;
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Create the interop host control.
+            System.Windows.Forms.Integration.WindowsFormsHost host =
+                new System.Windows.Forms.Integration.WindowsFormsHost();
+
+            // Create the Renderer 3D control.
+            Renderer3D renderer = new Renderer3D();
+
+            // Assign the Renderer 3D control as the host control's child.
+            host.Child = renderer;
+
+            // Add the interop host control to the Grid 
+            // control's collection of child controls. 
+            this.MainGrid.Children.Add(host);
+
+
+            Range range = new Range(-150, 150);
+            int steps = 50;
+
+            Shape surface = Builder.buildOrthonomal(new OrthonormalGrid(range, steps, range, steps), new MyMapper());
+            surface.ColorMapper = new ColorMapper(new ColorMapRainbow(), surface.Bounds.zmin, surface.Bounds.zmax, new Color(1, 1, 1, 0.8));
+            surface.FaceDisplayed = true;
+            surface.WireframeDisplayed = true;
+            surface.WireframeColor = Color.CYAN;
+            surface.WireframeColor.mul(new Color(1, 1, 1, 0.5));
+
+            //surface.LegendDisplayed = True
+
+            Chart chart = new Chart(renderer, Quality.Nicest);
+            chart.Scene.Graph.Add(surface);
+            chart.AxeLayout.XTickLabelDisplayed = true;
+            chart.AxeLayout.YTickLabelDisplayed = true;
+            chart.AxeLayout.ZTickLabelDisplayed = true;
+
+            nzy3D.Chart.Controllers.Mouse.Camera.CameraMouseController mouse = new nzy3D.Chart.Controllers.Mouse.Camera.CameraMouseController();
+            mouse.addControllerEventListener(renderer);
+            chart.addController(mouse);
+
+            // This is just to ensure code is reentrant (used when code is not in Form_Load but another reentrant event)
+            DisposeBackgroundThread();
+
+            t = new nzy3D.Chart.Controllers.Thread.Camera.CameraThreadController();
+            t.addControllerEventListener(renderer);
+            mouse.addSlaveThreadController(t);
+            chart.addController(t);
+            t.Start();
+
+            renderer.setView(chart.View);
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            DisposeBackgroundThread();
+        }
+
+        private void DisposeBackgroundThread()
+        {
+            if ((t != null))
+            {
+                t.Dispose();
+            }
+        }
+
+    }
+}
---
+++ nzy3d-wpfDemo/MyMapper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace nzy3d_wpfDemo
+{
+    class MyMapper : nzy3D.Plot3D.Builder.Mapper
+    {
+
+        public override double f(double x, double y)
+        {
+            return 10 * Math.Sin(x / 10) * Math.Cos(y / 20) * x;
+        }
+
+    }
+}
