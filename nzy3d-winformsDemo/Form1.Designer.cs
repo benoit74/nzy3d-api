624b963a8a6c7250ba35091d85b788d2787cdb71
benoit74 <benoit.beraud@oviles.info>
2015-01-08 10:11:18 +0100

    New Winforms Demo

---
+++ nzy3d-winformsDemo/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace nzy3d_winformsDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.chkDisplayXTicks = new System.Windows.Forms.CheckBox();
+            this.chkDisplayYTicks = new System.Windows.Forms.CheckBox();
+            this.chkDisplayZTick = new System.Windows.Forms.CheckBox();
+            this.chkDisplayZAxisLabel = new System.Windows.Forms.CheckBox();
+            this.chkDisplayYAxisLabel = new System.Windows.Forms.CheckBox();
+            this.chkDisplayXAxisLabel = new System.Windows.Forms.CheckBox();
+            this.chkDisplayTickLines = new System.Windows.Forms.CheckBox();
+            this.myRenderer3D = new nzy3D.Plot3D.Rendering.View.Renderer3D();
+            this.SuspendLayout();
+            // 
+            // chkDisplayXTicks
+            // 
+            this.chkDisplayXTicks.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.chkDisplayXTicks.AutoSize = true;
+            this.chkDisplayXTicks.Checked = true;
+            this.chkDisplayXTicks.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkDisplayXTicks.Location = new System.Drawing.Point(553, 12);
+            this.chkDisplayXTicks.Name = "chkDisplayXTicks";
+            this.chkDisplayXTicks.Size = new System.Drawing.Size(99, 17);
+            this.chkDisplayXTicks.TabIndex = 1;
+            this.chkDisplayXTicks.Text = "Display X Ticks";
+            this.chkDisplayXTicks.UseVisualStyleBackColor = true;
+            this.chkDisplayXTicks.CheckedChanged += new System.EventHandler(this.checkBoxes_CheckedChanged);
+            // 
+            // chkDisplayYTicks
+            // 
+            this.chkDisplayYTicks.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.chkDisplayYTicks.AutoSize = true;
+            this.chkDisplayYTicks.Checked = true;
+            this.chkDisplayYTicks.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkDisplayYTicks.Location = new System.Drawing.Point(553, 35);
+            this.chkDisplayYTicks.Name = "chkDisplayYTicks";
+            this.chkDisplayYTicks.Size = new System.Drawing.Size(99, 17);
+            this.chkDisplayYTicks.TabIndex = 2;
+            this.chkDisplayYTicks.Text = "Display Y Ticks";
+            this.chkDisplayYTicks.UseVisualStyleBackColor = true;
+            this.chkDisplayYTicks.CheckedChanged += new System.EventHandler(this.checkBoxes_CheckedChanged);
+            // 
+            // chkDisplayZTick
+            // 
+            this.chkDisplayZTick.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.chkDisplayZTick.AutoSize = true;
+            this.chkDisplayZTick.Checked = true;
+            this.chkDisplayZTick.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkDisplayZTick.Location = new System.Drawing.Point(553, 58);
+            this.chkDisplayZTick.Name = "chkDisplayZTick";
+            this.chkDisplayZTick.Size = new System.Drawing.Size(99, 17);
+            this.chkDisplayZTick.TabIndex = 3;
+            this.chkDisplayZTick.Text = "Display Z Ticks";
+            this.chkDisplayZTick.UseVisualStyleBackColor = true;
+            this.chkDisplayZTick.CheckedChanged += new System.EventHandler(this.checkBoxes_CheckedChanged);
+            // 
+            // chkDisplayZAxisLabel
+            // 
+            this.chkDisplayZAxisLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.chkDisplayZAxisLabel.AutoSize = true;
+            this.chkDisplayZAxisLabel.Checked = true;
+            this.chkDisplayZAxisLabel.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkDisplayZAxisLabel.Location = new System.Drawing.Point(553, 127);
+            this.chkDisplayZAxisLabel.Name = "chkDisplayZAxisLabel";
+            this.chkDisplayZAxisLabel.Size = new System.Drawing.Size(116, 17);
+            this.chkDisplayZAxisLabel.TabIndex = 6;
+            this.chkDisplayZAxisLabel.Text = "Display Z axis label";
+            this.chkDisplayZAxisLabel.UseVisualStyleBackColor = true;
+            this.chkDisplayZAxisLabel.CheckedChanged += new System.EventHandler(this.checkBoxes_CheckedChanged);
+            // 
+            // chkDisplayYAxisLabel
+            // 
+            this.chkDisplayYAxisLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.chkDisplayYAxisLabel.AutoSize = true;
+            this.chkDisplayYAxisLabel.Checked = true;
+            this.chkDisplayYAxisLabel.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkDisplayYAxisLabel.Location = new System.Drawing.Point(553, 104);
+            this.chkDisplayYAxisLabel.Name = "chkDisplayYAxisLabel";
+            this.chkDisplayYAxisLabel.Size = new System.Drawing.Size(116, 17);
+            this.chkDisplayYAxisLabel.TabIndex = 5;
+            this.chkDisplayYAxisLabel.Text = "Display Y axis label";
+            this.chkDisplayYAxisLabel.UseVisualStyleBackColor = true;
+            this.chkDisplayYAxisLabel.CheckedChanged += new System.EventHandler(this.checkBoxes_CheckedChanged);
+            // 
+            // chkDisplayXAxisLabel
+            // 
+            this.chkDisplayXAxisLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.chkDisplayXAxisLabel.AutoSize = true;
+            this.chkDisplayXAxisLabel.Checked = true;
+            this.chkDisplayXAxisLabel.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkDisplayXAxisLabel.Location = new System.Drawing.Point(553, 81);
+            this.chkDisplayXAxisLabel.Name = "chkDisplayXAxisLabel";
+            this.chkDisplayXAxisLabel.Size = new System.Drawing.Size(116, 17);
+            this.chkDisplayXAxisLabel.TabIndex = 4;
+            this.chkDisplayXAxisLabel.Text = "Display X axis label";
+            this.chkDisplayXAxisLabel.UseVisualStyleBackColor = true;
+            this.chkDisplayXAxisLabel.CheckedChanged += new System.EventHandler(this.checkBoxes_CheckedChanged);
+            // 
+            // chkDisplayTickLines
+            // 
+            this.chkDisplayTickLines.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.chkDisplayTickLines.AutoSize = true;
+            this.chkDisplayTickLines.Checked = true;
+            this.chkDisplayTickLines.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkDisplayTickLines.Location = new System.Drawing.Point(553, 150);
+            this.chkDisplayTickLines.Name = "chkDisplayTickLines";
+            this.chkDisplayTickLines.Size = new System.Drawing.Size(104, 17);
+            this.chkDisplayTickLines.TabIndex = 7;
+            this.chkDisplayTickLines.Text = "Display tick lines";
+            this.chkDisplayTickLines.UseVisualStyleBackColor = true;
+            this.chkDisplayTickLines.CheckedChanged += new System.EventHandler(this.checkBoxes_CheckedChanged);
+            // 
+            // myRenderer3D
+            // 
+            this.myRenderer3D.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.myRenderer3D.BackColor = System.Drawing.Color.Black;
+            this.myRenderer3D.Location = new System.Drawing.Point(12, 12);
+            this.myRenderer3D.Name = "myRenderer3D";
+            this.myRenderer3D.Size = new System.Drawing.Size(523, 408);
+            this.myRenderer3D.TabIndex = 9;
+            this.myRenderer3D.VSync = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(686, 432);
+            this.Controls.Add(this.myRenderer3D);
+            this.Controls.Add(this.chkDisplayTickLines);
+            this.Controls.Add(this.chkDisplayZAxisLabel);
+            this.Controls.Add(this.chkDisplayYAxisLabel);
+            this.Controls.Add(this.chkDisplayXAxisLabel);
+            this.Controls.Add(this.chkDisplayZTick);
+            this.Controls.Add(this.chkDisplayYTicks);
+            this.Controls.Add(this.chkDisplayXTicks);
+            this.Name = "Form1";
+            this.Text = "nzy3d - Winforms Sample";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox chkDisplayXTicks;
+        private System.Windows.Forms.CheckBox chkDisplayYTicks;
+        private System.Windows.Forms.CheckBox chkDisplayZTick;
+        private System.Windows.Forms.CheckBox chkDisplayZAxisLabel;
+        private System.Windows.Forms.CheckBox chkDisplayYAxisLabel;
+        private System.Windows.Forms.CheckBox chkDisplayXAxisLabel;
+        private System.Windows.Forms.CheckBox chkDisplayTickLines;
+        private nzy3D.Plot3D.Rendering.View.Renderer3D myRenderer3D;
+    }
+}
+
---
+++ nzy3d-winformsDemo/Form1.cs
@@ -0,0 +1,265 @@
+﻿using nzy3D.Chart;
+using nzy3D.Chart.Controllers.Thread.Camera;
+using nzy3D.Colors;
+using nzy3D.Colors.ColorMaps;
+using nzy3D.Maths;
+using nzy3D.Plot3D.Builder;
+using nzy3D.Plot3D.Builder.Concrete;
+using nzy3D.Plot3D.Primitives;
+using nzy3D.Plot3D.Primitives.Axes.Layout;
+using nzy3D.Plot3D.Rendering.Canvas;
+using nzy3D.Plot3D.Rendering.View;
+using nzy3d_wpfDemo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace nzy3d_winformsDemo
+{
+    public partial class Form1 : Form
+    {
+
+        private CameraThreadController t;
+        private IAxeLayout axeLayout;
+
+        public Form1()
+        {
+            InitializeComponent();
+            //InitRenderer();
+        }
+
+        private void InitRenderer()
+        {
+
+            // Create the Renderer 3D control.
+            //Renderer3D myRenderer3D = new Renderer3D();
+
+            // Add the Renderer control to the panel
+           // mainPanel.Controls.Clear();
+            //mainPanel.Controls.Add(myRenderer3D);
+
+            // Create a range for the graph generation
+            Range range = new Range(-150, 150);
+            int steps = 50;
+
+            // Build a nice surface to display with cool alpha colors 
+            // (alpha 0.8 for surface color and 0.5 for wireframe)
+            Shape surface = Builder.buildOrthonomal(new OrthonormalGrid(range, steps, range, steps), new MyMapper());
+            surface.ColorMapper = new ColorMapper(new ColorMapRainbow(), surface.Bounds.zmin, surface.Bounds.zmax, new Color(1, 1, 1, 0.8));
+            surface.FaceDisplayed = true;
+            surface.WireframeDisplayed = true;
+            surface.WireframeColor = Color.CYAN;
+            surface.WireframeColor.mul(new Color(1, 1, 1, 0.5));
+
+            // Create the chart and embed the surface within
+            Chart chart = new Chart(myRenderer3D, Quality.Nicest);
+            chart.Scene.Graph.Add(surface);
+            axeLayout = chart.AxeLayout;
+            
+            // All activated by default
+            DisplayXTicks = true;
+            DisplayXAxisLabel = true;
+            DisplayYTicks = true;
+            DisplayYAxisLabel = true;
+            DisplayZTicks = true;
+            DisplayZAxisLabel = true;
+            DisplayTickLines = true;
+
+            // Create a mouse control
+            nzy3D.Chart.Controllers.Mouse.Camera.CameraMouseController mouse = new nzy3D.Chart.Controllers.Mouse.Camera.CameraMouseController();
+            mouse.addControllerEventListener(myRenderer3D);
+            chart.addController(mouse);
+
+            // This is just to ensure code is reentrant (used when code is not called in Form_Load but another reentrant event)
+            DisposeBackgroundThread();
+
+            // Create a thread to control the camera based on mouse movements
+            t = new nzy3D.Chart.Controllers.Thread.Camera.CameraThreadController();
+            t.addControllerEventListener(myRenderer3D);
+            mouse.addSlaveThreadController(t);
+            chart.addController(t);
+            t.Start();
+
+            // Associate the chart with current control
+            myRenderer3D.setView(chart.View);
+
+            this.Refresh();
+        }
+        
+        private void DisposeBackgroundThread()
+        {
+            if ((t != null))
+            {
+                t.Dispose();
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DisposeBackgroundThread();
+        }
+
+
+        private bool _DisplayTickLines;
+        public bool DisplayTickLines
+        {
+            get
+            {
+                return _DisplayTickLines;
+            }
+            set
+            {
+                _DisplayTickLines = value;
+                if (axeLayout != null)
+                {
+                    axeLayout.TickLineDisplayed = value;
+                }
+            }
+        }
+
+        private bool _DisplayXTicks;
+        public bool DisplayXTicks
+        {
+            get
+            {
+                return _DisplayXTicks;
+            }
+            set
+            {
+                _DisplayXTicks = value;
+                if (axeLayout != null)
+                {
+                    axeLayout.XTickLabelDisplayed = value;
+                }
+            }
+        }
+
+        private bool _DisplayYTicks;
+        public bool DisplayYTicks
+        {
+            get
+            {
+                return _DisplayYTicks;
+            }
+            set
+            {
+                _DisplayYTicks = value;
+                if (axeLayout != null)
+                {
+                    axeLayout.YTickLabelDisplayed = value;
+                }
+            }
+        }
+
+        private bool _DisplayZTicks;
+        public bool DisplayZTicks
+        {
+            get
+            {
+                return _DisplayZTicks;
+            }
+            set
+            {
+                _DisplayZTicks = value;
+                if (axeLayout != null)
+                {
+                    axeLayout.ZTickLabelDisplayed = value;
+                }
+            }
+        }
+
+        private bool _DisplayXAxisLabel;
+        public bool DisplayXAxisLabel
+        {
+            get
+            {
+                return _DisplayXAxisLabel;
+            }
+            set
+            {
+                _DisplayXAxisLabel = value;
+                if (axeLayout != null)
+                {
+                    axeLayout.XAxeLabelDisplayed = value;
+                }
+            }
+        }
+
+        private bool _DisplayYAxisLabel;
+        public bool DisplayYAxisLabel
+        {
+            get
+            {
+                return _DisplayYAxisLabel;
+            }
+            set
+            {
+                _DisplayYAxisLabel = value;
+                if (axeLayout != null)
+                {
+                    axeLayout.YAxeLabelDisplayed = value;
+                }
+            }
+        }
+
+        private bool _DisplayZAxisLabel;
+        public bool DisplayZAxisLabel
+        {
+            get
+            {
+                return _DisplayZAxisLabel;
+            }
+            set
+            {
+                _DisplayZAxisLabel = value;
+                if (axeLayout != null)
+                {
+                    axeLayout.ZAxeLabelDisplayed = value;
+                }
+            }
+        }
+
+        private void checkBoxes_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkDisplayXTicks.Checked != DisplayXTicks)
+            {
+                DisplayXTicks = chkDisplayXTicks.Checked;
+            }
+            if (chkDisplayYTicks.Checked != DisplayYTicks)
+            {
+                DisplayYTicks = chkDisplayYTicks.Checked;
+            }
+            if (chkDisplayZTick.Checked != DisplayZTicks)
+            {
+                DisplayZTicks = chkDisplayZTick.Checked;
+            }
+            if (chkDisplayXAxisLabel.Checked != DisplayXAxisLabel)
+            {
+                DisplayXAxisLabel = chkDisplayXAxisLabel.Checked;
+            }
+            if (chkDisplayYAxisLabel.Checked != DisplayYAxisLabel)
+            {
+                DisplayYAxisLabel = chkDisplayYAxisLabel.Checked;
+            }
+            if (chkDisplayZAxisLabel.Checked != DisplayZAxisLabel)
+            {
+                DisplayZAxisLabel = chkDisplayZAxisLabel.Checked;
+            }
+            if (chkDisplayTickLines.Checked != DisplayTickLines)
+            {
+                DisplayTickLines = chkDisplayTickLines.Checked;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InitRenderer();
+        }
+
+    }
+}
